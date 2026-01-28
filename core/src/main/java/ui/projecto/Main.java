384f58b65b790523788163be7a68e5b8d2ced0ef
Mario <mariodavidtaipeflores@elpuig.xeill.net>
2026-01-28 20:23:27 +0100

    Prueba

--- core/src/main/java/ui/projecto/Main.java
+++ core/src/main/java/ui/projecto/Main.java
@@ -48,7 +48,7 @@ public class Main extends ApplicationAdapter {
             toggleFullscreen();
         }
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         viewport.apply();
