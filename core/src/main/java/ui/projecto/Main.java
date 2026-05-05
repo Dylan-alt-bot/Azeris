9b226a9385b2b3c1f54fd1d9b3da4eafefb504b4
Mario <mariodavidtaipeflores@elpuig.xeill.net>
2026-05-05 16:53:48 +0200

    Más mapas y sonido puerta

--- core/src/main/java/ui/projecto/Main.java
+++ core/src/main/java/ui/projecto/Main.java
@@ -81,7 +81,7 @@ public class Main extends ApplicationAdapter {
         uiCamera = new OrthographicCamera();
         uiCamera.setToOrtho(false, ConstantsPlayer.VIRTUAL_WIDTH, ConstantsPlayer.VIRTUAL_HEIGHT);
 
-        mapManager = new MapManager("maps/mapas/Mapa2.tmx");
+        mapManager = new MapManager("maps/mapas/Mapa4.tmx");
         playerSpawn = mapManager.getRandomPlayerSpawn();
         jugadorPrincipal = new Player(playerSpawn.x, playerSpawn.y, mapManager);
         playerUI = new PlayerUI(jugadorPrincipal);
